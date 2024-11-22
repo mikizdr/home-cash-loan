2f736659d1e89b0060d6cc3cb786cfe4618b7642
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2024-11-22 18:39:01 +0100

    Add ClientCreateRequest for client validation rules and messages

---
+++ app/Http/Requests/ClientCreateRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClientCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => ['required', 'string', 'min:3', 'max:50'],
+            'last_name' => ['required', 'string', 'min:3', 'max:50'],
+            'email' => ['nullable', 'string', 'lowercase', 'email', 'max:255', 'unique:clients', 'required_without:phone'],
+            'phone' => ['nullable', 'string', 'max:30', 'unique:clients', 'required_without:email'],
+            'address' => ['nullable', 'string', 'max:255'],
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required_without' => __('The email field is required when phone is not provided.'),
+            'phone.required_without' => __('The phone field is required when email is not provided.'),
+        ];
+    }
+}
